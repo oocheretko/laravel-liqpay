b4c5ef3573b737ef7b4c92a0a632903e0c8215c1
Your Name <you@example.com>
2022-11-09 16:45:43 +0200

    fix

--- src/Support/LiqPay.php
+++ src/Support/LiqPay.php
@@ -49,7 +49,7 @@ class LiqPay
     public function __construct(
         string $_checkout_url = 'https://www.liqpay.ua/api/3/checkout',
         string|NULL $_server_response_code = null,
-        string $_supportedCurrencies = array(
+        $_supportedCurrencies = array(
             self::CURRENCY_EUR,
             self::CURRENCY_USD,
             self::CURRENCY_UAH,
